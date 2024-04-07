8afe90920b05761cd02bc931fa40158e12e1d569
Kelvin Abaidoo <kabaidoo@hotmail.com>
2024-04-07 13:30:23 +0000

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by kobby on 07/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
