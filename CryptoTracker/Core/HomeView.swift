91b4b492c32728831d216f271d66f4f537a9942a
Kelvin Abaidoo <kabaidoo@hotmail.com>
2025-01-14 08:47:27 +0000

    Add PieChartView

--- CryptoTracker/Core/HomeView.swift
+++ CryptoTracker/Core/HomeView.swift
@@ -29,7 +29,6 @@ struct HomeView: View {
         
             }
          
-            
         }
         
     }
---
+++ CryptoTracker/Core/Portfolio/Views/PieChartView.swift
@@ -0,0 +1,18 @@
+//
+//  ChartView.swift
+//  CryptoTracker
+//
+//  Created by kobby on 14/01/2025.
+//
+
+import SwiftUI
+
+struct PieChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PieChartView()
+}
